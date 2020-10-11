de4ade23f0127cc20492a97f8965da5ce8197b1a
Mark Szavin <szavinm@yahoo.com>
2020-10-11 20:04:57 +0200

    draw ball, paddle, score

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
 const rulesBtn = document.querySelector("#rules-btn");
 const closeBtn = document.querySelector("#close-btn");
 const rules = document.querySelector("#rules");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+let score = 0;
+
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dirX: 4,
+  dirY: -4,
+};
+
+const drawBall = () => {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.x, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+};
+
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dirX: 0,
+};
+
+const drawPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+};
+
+const drawScore = () => {
+  ctx.font = "20px Arial";
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+};
+
+const drawBallPaddleScore = () => {
+  drawBall();
+  drawPaddle();
+  drawScore();
+};
+
+drawBallPaddleScore();
 
 rulesBtn.addEventListener("click", () => {
   rules.classList.add("show");
