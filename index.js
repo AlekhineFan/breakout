9c8189bed294672c2739e926a63afb661794d581
Mark Szavin <szavinm@yahoo.com>
2020-10-12 08:49:03 +0200

    draw bricks

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 let score = 0;
 
+const brickRowCount = 9;
+const brickColCount = 5;
+const bricks = [];
+
+const brickProprs = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 65,
+  visible: true,
+};
+
 const ball = {
   x: canvas.width / 2,
   y: canvas.height / 2,
@@ -16,7 +29,7 @@ const ball = {
 
 const drawBall = () => {
   ctx.beginPath();
-  ctx.arc(ball.x, ball.x, ball.size, 0, Math.PI * 2);
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
   ctx.fillStyle = "#0095dd";
   ctx.fill();
   ctx.closePath();
@@ -51,6 +64,32 @@ const drawBallPaddleScore = () => {
 };
 
 drawBallPaddleScore();
+
+const createBricks = () => {
+  for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = [];
+    for (let j = 0; j < brickColCount; j++) {
+      const x = i * (brickProprs.w + brickProprs.padding) + brickProprs.offsetX;
+      const y = j * (brickProprs.h + brickProprs.padding) + brickProprs.offsetY;
+      bricks[i][j] = { x, y, ...brickProprs };
+    }
+  }
+};
+
+const drawBricks = () => {
+  bricks.forEach((col) => {
+    col.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+};
+
+createBricks();
+drawBricks();
 
 rulesBtn.addEventListener("click", () => {
   rules.classList.add("show");
